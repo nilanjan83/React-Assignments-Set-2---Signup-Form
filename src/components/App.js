66c972526226e49abd148339e7287fd426429148
nilanjan83 <68443233+nilanjan83@users.noreply.github.com>
2020-12-22 02:08:31 +0530

    chan

--- src/components/App.js
+++ src/components/App.js
@@ -75,7 +75,7 @@ export default class App extends React.Component {
         <div id="main"></div>
         <div>
           {this.state.error && <div>{this.state.error}</div>}
-          {this.state.username && <div>Hello{this.state.username}</div>}
+          {this.state.username && <div>Hello {this.state.username}</div>}
         </div>
         <label>Name</label>
         <input
